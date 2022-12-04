887a1d7265d74f6de22f7219233dca73e07caa80
badtux <jeftenunes@hotmail.com>
2022-12-04 18:42:52 -0500

    first elixir scripts - learning about modules

---
+++ module_playground.exs
@@ -0,0 +1,14 @@
+defmodule ModulePlayground do
+  import IO, only: [puts: 1]
+  import Kernel, except: [inspect: 1]
+
+  def say_hello do
+    inspect("HelloWorld")
+  end
+
+  def inspect(param1) do
+    puts("Starting output")
+    puts(param1)
+    puts("Ending output")
+  end
+end

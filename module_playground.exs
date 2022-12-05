ac08cc1c23006f08e3e1d5ad8feea045a7e831d5
jeftenunes <jeftenunes@hotmail.com>
2022-12-04 19:01:54 -0500

    require modules

--- module_playground.exs
+++ module_playground.exs
@@ -2,6 +2,9 @@ defmodule ModulePlayground do
   import IO, only: [puts: 1]
   import Kernel, except: [inspect: 1]
 
+  require Integer
+  alias ModulePlayground.Misc.Util.Math, as: CustomMath
+
   def say_hello do
     inspect("HelloWorld")
   end
@@ -10,5 +13,14 @@ defmodule ModulePlayground do
     puts("Starting output")
     puts(param1)
     puts("Ending output")
+  end
+
+  def print_sum do
+    CustomMath.add(1, 2)
+  end
+
+  def print_is_even(num) do
+    is_num_even = Integer.is_even(num)
+    puts("Is the number #{num} even? #{num} is#{(is_num_even && " ") || " not "}even.")
   end
 end

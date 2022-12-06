bb2f3ae82ff7468a0136674888e311bf8bb40fef
jeftenunes <jeftenunes@hotmail.com>
2022-12-05 21:23:37 -0500

    custom functions, private functions, functions..

--- enum.exs
+++ enum.exs
@@ -1,4 +1,20 @@
 defmodule Sample.Enum do
-  def first([]), do: nil
-  def first([head | _]), do: head
+  import IO, only: [puts: 1]
+  # quando criamos 2 funcoes c o mesmo nome,
+  # uma c 1 parametro obrigatorio e um c 1 parametro obrigatorio e outro default,
+  # temos q declarar a funcao c parametro
+  # default dps da q tem 2 parametros obrigatorios
+
+  def first(list, val \\ nil)
+  def first([], val), do: val
+  def first([head | _], _), do: head
+
+  def add(list, val \\ 0) do
+    trace(val)
+    [val | list]
+  end
+
+  defp trace(str) do
+    puts("Params passed was #{str}")
+  end
 end
---
+++ utils.exs
@@ -0,0 +1,5 @@
+defmodule Sample.Utils do
+  def custom_fn(param1, f) do
+    f.(param1)
+  end
+end
